fa3da123cbe921b0e10ef0d21a06eea7076bf39a
Peter-Immanuel <peterbemshima@gmail.com>
2024-07-25 23:15:43 +0100

    Feat: Setup domain types

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Peter-Immanuel/fox-alpine
+
+go 1.21.1
---
+++ pkg/domain/pet.go
@@ -0,0 +1,33 @@
+package domain
+
+type Contact struct {
+	Owner string `json:"owner" bson:"owner"`
+	Phone string `json:"phone" bson:"phone"`
+	City  string `json:"city" bson:"city"`
+	State string `json:"state" bson:"state"`
+}
+
+type Pet struct {
+	ID       int     `json:"id" bson:"_id,omitempty"`
+	Category string  `json:"category" bson:"category"`
+	Breed    string  `json:"bread" bson:"breed,omitempty"`
+	Age      int     `json:"age" bson:"age"`
+	Gender   string  `json:"gender" bson:"gender"`
+	Colors   string  `json:"colors" bson:"colors,omitempty"`
+	Contact  Contact `json:"contact" bson:"contact,omitempty"`
+	Price    float64 `json:"price" bson:"price,omitempty"`
+}
+
+type PetService interface {
+	Get(id int) (*Pet, error)
+	List(query ...string) ([]*Pet, error)
+	Create(p *Pet) error
+	Delete(id int) error
+}
+
+type PetDB interface {
+	Get(id int) (*Pet, error)
+	List(query ...string) ([]*Pet, error)
+	Create(p *Pet) error
+	Delete(id int) error
+}
